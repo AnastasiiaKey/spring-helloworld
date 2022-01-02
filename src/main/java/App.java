b33f399518e2b6f7cad165ba7063ba32a2153ce1
AnastasiiaKey <KochelaevskayaNastya@yandex.ru>
2022-01-02 14:49:50 +0300

    Task completed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,18 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
+
+        boolean comparator = bean==bean1;
+        System.out.println(comparator);
+
+        boolean comparator1 = bean3==bean4;
+        System.out.println(comparator1);
+
     }
 }
